9bdbffc86f92dd067677e3125e177bc1625fb0e4
Antonio Vilar <tonivilar@gmail.com>
2015-04-23 18:04:05 +0200

    Simplified isPrime method

--- prime-calculator/src/main/scala/PrimeCalculator.scala
+++ prime-calculator/src/main/scala/PrimeCalculator.scala
@@ -1,6 +1,6 @@
 object PrimeCalculator {
   def generatePrimesUpTo(number: Int): List[Int] = {
-    def isPrime(x: Int): Boolean = 2 to x find { x % _ == 0 } equals Some(x)
+    def isPrime(x: Int): Boolean = 2 until x forall { x % _ != 0 }
     (2 to number filter isPrime).toList
   }
 }
